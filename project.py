bb06a72876ee6eaeb78c36dece1ddefdf5fe639d
Dillon Barnes <dillonb07dev@gmail.com>
2022-11-02 13:43:44 +0000

    Few changes

--- project.py
+++ project.py
@@ -1,13 +1,13 @@
 import json
 import os
-import sqlite
+import sqlite3
 
 PRICES = {
         "Carpets": 22.50,
         "Underlay - First Step": 5.99,
         "Underlay - Monarch": 7.99,
         "Underlay - Royal": 60,
-        "Gripper": 1.10
+        "Gripper": 1.10,
         "hour": 65
     }
 
@@ -23,7 +23,7 @@ def add_customer(customer):
     customer['town'] = input('Town: ')
     customer['telephone'] = input('Telephone number: ')
 
-    cursor.execute(f'INSERT INTO Customers ({customer["forename"], {customer["surname"]}, {customer["town"]}, {customer["telephone"]})')
+    cursor.execute(f'INSERT INTO Customers ({customer["forename"]}, {customer["surname"]}, {customer["town"]}, {customer["telephone"]})')
     db.commit()
 
 
