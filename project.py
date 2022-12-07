fcdcaf0ae58d3e1f4c3736943cba056dc5a03483
Dillon Barnes <dillonb07dev@gmail.com>
2022-12-07 13:43:44 +0000

    Export data to JSON file

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
+import csv
 import sqlite3
 import sys
 import math
+import json
 
 PRICES = [
     ["Carpets", 22.50],
@@ -37,9 +39,9 @@ def add_customer():
 def list_customers():
     """List all customers"""
     cursor.execute("SELECT * FROM Customers")
-    print("Forename | Surname | Town | Telephone")
+    print(f"{'Forename':10} | {'Surname':10} | {'Town':10} | Telephone")
     for row in cursor.fetchall():
-        print(f"{row[0]} | {row[1]} | {row[2]} | {row[3]}")
+        print(f"{row[0]:10} | {row[1]:10} | {row[2]:10} | {row[3]}")
 
 
 def add_quote():
@@ -58,7 +60,9 @@ def add_quote():
     for i, item in enumerate(PRICES):
         print(f"{i + 1}) {item[0]}")
     quote["underlay"] = int(input("Select an underlay: ")) - 1
-    quote["total_price"] = (PRICES[quote['underlay']][1] * quote['square_metres']) + (math.ceil(quote['square_metres'] / AMOUNT_PER_HOUR) * PRICE_PER_HOUR)
+    quote["total_price"] = (PRICES[quote["underlay"]][1] * quote["square_metres"]) + (
+        math.ceil(quote["square_metres"] / AMOUNT_PER_HOUR) * PRICE_PER_HOUR
+    )
 
     cursor.execute(
         f"""
@@ -72,16 +76,48 @@ VALUES (\'{quote["customer"]}\', \'{quote["length"]}\', \'{quote["width"]}\', \'
 def list_quotes():
     """List all quotes"""
     cursor.execute("SELECT * FROM Quotes")
-    print("Name | Length | Width | Square Metres | Underlay | Total Price")
+    print(
+        f"{'Name':10} | {'Length':10} | {'Width':10} | {'Square Metres':10} | {'Underlay':10} | Total Price"
+    )
     for row in cursor.fetchall():
-        cursor.execute(f"SELECT forename, surname FROM Customers WHERE ROWID = {row[0]}")
+        cursor.execute(
+            f"SELECT forename, surname FROM Customers WHERE ROWID = {row[0]}"
+        )
         customer = cursor.fetchone()
-        print(f"{customer[0]} {customer[1]} | {row[1]}m | {row[2]}m | {int(row[1]) * int(row[2])}m^2| {row[3]} | £{row[4]}")
+        print(
+            f"{customer[0] + ' ' + customer[1]:10} | {row[1] + 'm':10} | {row[2] + 'm':10} | {str(int(row[1]) * int(row[2])) + 'm^2':10}| {row[3]:10} | £{row[4]}"
+        )  # NOQA
+
 
 def export():
     """Export the database to a CSV file"""
-    # Export data to CSV file
-    pass
+    data = {"users": []}
+    cursor.execute("SELECT *, ROWID FROM Customers")
+    for row in cursor.fetchall():
+        # print(f"{row=}")
+        user = {
+                "surname": row[1],
+                "forename": row[0],
+                "town": row[2],
+                "telephone": row[3],
+                "quotes": []
+            }
+        cursor.execute(f"SELECT * FROM Quotes WHERE customer = {row[-1]}")
+        for quote in cursor.fetchall():
+            # print(f"{quote=}")
+            user['quotes'].append({"length": int(quote[0]),
+                "width": int(quote[1]),
+                "square_metres": int(quote[0]) * int(quote[1]),
+                "underlay": quote[2],
+                "total_price": int(quote[3]),
+            })
+        data['users'].append(user)
+    print(f"{data=}")
+    json.dump(data, open("data.json", "w", encoding='utf-8'), indent=4)
+    # with open('test.csv', 'w') as f:
+    #     for key in my_dict.keys():
+    #         f.write("%s,%s"%(key,my_dict[key])) # NOQA
+
 
 def menu():
     """Display the menu"""
@@ -97,23 +133,33 @@ def menu():
     """
         )
         option = int(input(">>> "))
-        if option == 1:
-            add_customer()
-        elif option == 2:
-            list_customers()
-        elif option == 3:
-            add_quote()
-        elif option == 4:
-            list_quotes()
-        elif option == 5:
-            pass
-        elif option == 6:
-            sys.exit()
+        options = {
+            1: add_customer,
+            2: list_customers,
+            3: add_quote,
+            4: list_quotes,
+            5: export,
+            6: sys.exit,
+        }
+        options[option]()
+        # if option == 1:
+        #     add_customer()
+        # elif option == 2:
+        #     list_customers()
+        # elif option == 3:
+        #     add_quote()
+        # elif option == 4:
+        #     list_quotes()
+        # elif option == 5:
+        #     export()
+        # elif option == 6:
+        #     sys.exit()
     except ValueError:
         print("Invalid option")
         menu()
 
 
+# Using options dictionary, call the function
+
 while True:
     menu()
-
